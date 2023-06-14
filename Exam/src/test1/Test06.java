2f8731e20fcc5aebb29323fa1e3954ae375ee2a2
slothfkdla <slothfkdla@gmail.com>
2023-06-14 09:43:02 +0900

    오탈자 수정

--- Exam/src/test1/Test06.java
+++ Exam/src/test1/Test06.java
@@ -5,42 +5,21 @@ import java.util.Scanner;
 /*
  * 날짜 : 2023/06/13
  * 이름 : 신진성 
- * 내용 : 자바 삼항연산자 연습문제
+ * 내용 : 자바 조건문 연습문제
  */
 public class Test06 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("점수 입력 : ");
-		int score = sc.nextInt();
+		System.out.print("x값 입력 : ");
+		int x = sc.nextInt();
 
-		System.out.println("입력한 점수는 " + score + "입니다.");
+		System.out.print("y값 입력 : ");
+		int y = sc.nextInt();
 
-		switch (score) {
-		case 10: {
+		String result = x > y ? "x가 더 큽니다." : "y가 더 큽니다.";
 
-		}
-		case 9: {
-			System.out.println("A 입니다.");
-			break;
-		}
-		case 8: {
-			System.out.println("B 입니다.");
-			break;
-		}
-		case 7: {
-			System.out.println("C 입니다.");
-			break;
-		}
-		case 6: {
-			System.out.println("D 입니다.");
-			break;
-		}
-		default:
-			System.out.println("F 입니다.");
-			break;
-		}
-
+		System.out.println(result);
 		sc.close();
 	}
 }
