06c53b19b0e82cf5241bc3e5ece283befa494208
slothfkdla <slothfkdla@gmail.com>
2023-06-15 12:05:06 +0900

    자바 연습문제2

---
+++ Exam/src/test2/Test10.java
@@ -0,0 +1,21 @@
+package test2;
+
+/*
+ * 날짜 : 2023/06/15
+ * 이름 : 신진성 
+ * 내용 : 자바 피보나치 수열 재귀 메서드 연습문제
+ */
+public class Test10 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.print(fibo(i) + " ");
+		}
+	}
+
+	public static int fibo(int n) {
+		if (n <= 1) {
+			return n;
+		}
+		return fibo(n - 2) + fibo(n - 1);
+	}
+}
