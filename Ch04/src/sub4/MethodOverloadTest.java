4f33c542e0d3c8553b4e4ace2e5a8890d0d09116
slothfkdla <slothfkdla@gmail.com>
2023-06-15 10:38:23 +0900

    Java Method 실습

---
+++ Ch04/src/sub4/MethodOverloadTest.java
@@ -0,0 +1,37 @@
+package sub4;
+
+/*
+ * 날짜 : 2023/06/15
+ * 이름 : 신진성 
+ * 내용 : Java Method 오버로딩 실습하기 
+ */
+public class MethodOverloadTest {
+	public static void main(String[] args) {
+		int r1 = plus(1);
+		int r2 = plus(1, 2);
+		double r3 = plus(1.1, 2.3);
+		String r4 = plus("Hello ", "World");
+
+		System.out.println("r1 : " + r1);
+		System.out.println("r2 : " + r2);
+		System.out.println("r3 : " + r3);
+		System.out.println("r4 : " + r4);
+	}
+
+	// 오버로딩 Method 정의
+	public static int plus(int a) {
+		return a + a;
+	}
+
+	public static int plus(int a, int b) {
+		return a + b;
+	}
+
+	public static double plus(double a, double b) {
+		return a + b;
+	}
+
+	public static String plus(String a, String b) {
+		return a + b;
+	}
+}
