01007aa96d0c6f16e828c84bfa925f5f8e5a0c8f
slothfkdla <slothfkdla@gmail.com>
2023-06-19 12:58:50 +0900

    백준 4장 완료

--- CodingTest/src/step4/test10.java
+++ CodingTest/src/step4/test10.java
@@ -1,5 +1,32 @@
 package step4;
 
-public class test10 {
+import java.util.Scanner;
 
+public class test10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ct = sc.nextInt();
+		double[] arr = new double[ct];
+		double top = 0;
+
+		for (int i = 0; i < ct; i++) {
+			double a = sc.nextDouble();
+			arr[i] = a;
+		}
+
+		for (double n : arr) {
+			if (n > top) {
+				top = n;
+			}
+		}
+
+		double sum = 0;
+		for (double n : arr) {
+			n = n / top * 100;
+			sum += n;
+		}
+
+		System.out.println(sum / ct);
+		sc.close();
+	}
 }
