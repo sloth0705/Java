2a1011120a2ecf9dbcf3d404a97a62e05d53f41a
slothfkdla <slothfkdla@gmail.com>
2023-06-19 17:50:00 +0900

    5장 8까지

--- CodingTest/src/step5/Test07.java
+++ CodingTest/src/step5/Test07.java
@@ -1,5 +1,22 @@
 package step5;
 
-public class Test07 {
+import java.util.Scanner;
 
+public class Test07 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ct = sc.nextInt();
+		for (int i = 0; i < ct; i++) {
+			int count = sc.nextInt();
+			String str = sc.next();
+			String st = "";
+			for (int j = 0; j < str.length(); j++) {
+				for (int h = 0; h < count; h++) {
+					st += str.charAt(j);
+				}
+			}
+			System.out.println(st);
+		}
+		sc.close();
+	}
 }
--- CodingTest/src/step5/Test08.java
+++ CodingTest/src/step5/Test08.java
@@ -1,5 +1,17 @@
 package step5;
 
-public class Test08 {
+import java.util.Scanner;
 
+public class Test08 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine().trim();
+		String[] arr = str.split("\\s+");
+		int count = 0;
+		if (!arr[0].equals("")) {
+			count = arr.length;
+		}
+		System.out.println(count);
+		sc.close();
+	}
 }
