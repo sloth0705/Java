facdd35eb86753fc6bc09d543c88bd1a105af328
slothfkdla <slothfkdla@gmail.com>
2023-06-19 12:42:52 +0900

    백준 3-8, 3-9 완료

--- CodingTest/src/step4/test08.java
+++ CodingTest/src/step4/test08.java
@@ -1,5 +1,31 @@
 package step4;
 
-public class test08 {
+import java.util.Scanner;
 
+public class test08 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[10];
+		for (int i = 0; i < 10; i++) {
+			arr[i] = sc.nextInt() % 42;
+		}
+
+		int count = 10;
+		for (int i = 0; i < 42; i++) {
+			int count2 = 0;
+			for (int j = 0; j < 10; j++) {
+				if (arr[j] == i) {
+					count2++;
+				}
+			}
+			if (count2 > 1) {
+				count -= count2 - 1;
+			}
+			count2 = 0;
+		}
+
+		System.out.println(count);
+
+		sc.close();
+	}
 }
--- CodingTest/src/step4/test09.java
+++ CodingTest/src/step4/test09.java
@@ -1,5 +1,34 @@
 package step4;
 
-public class test09 {
+import java.util.Scanner;
 
+public class test09 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int[] arr = new int[a];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		}
+
+		for (int i = 0; i < b; i++) {
+			int c = sc.nextInt();
+			int d = sc.nextInt();
+
+			for (int j = 0; j < ((d - c) / 2 + 1); j++) {
+				int temp = 0;
+				temp = arr[d - 1 - j];
+				arr[d - 1 - j] = arr[c - 1 + j];
+				arr[c - 1 + j] = temp;
+			}
+		}
+
+		for (int n : arr) {
+			System.out.print(n + " ");
+		}
+
+		sc.close();
+	}
 }
