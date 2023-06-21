c4c5d226939aa278b52bdada55add5836783b416
slothfkdla <slothfkdla@gmail.com>
2023-06-21 10:36:51 +0900

    상속

---
+++ Ch05/src/sub5/Sedan.java
@@ -0,0 +1,20 @@
+package sub5;
+
+public class Sedan extends Car {
+
+	private int cc;
+
+	public Sedan(String name, String color, int speed, int cc) {
+		super(name, color, speed);
+		this.cc = cc;
+	}
+
+	public void speedTurbo() {
+		this.speed += 20;
+	}
+
+	public void show() {
+		super.show();
+		System.out.println("배기량 : " + this.cc);
+	}
+}
