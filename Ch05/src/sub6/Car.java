67e3d23347da29863a1b63c3ccd8c4b091eb8c76
slothfkdla <slothfkdla@gmail.com>
2023-06-21 11:51:23 +0900

    오버라이드

---
+++ Ch05/src/sub6/Car.java
@@ -0,0 +1,27 @@
+package sub6;
+
+public class Car {
+	protected String name;
+	protected String color;
+	protected int speed;
+
+	public Car(String name, String color, int speed) {
+		this.name = name;
+		this.color = color;
+		this.speed = speed;
+	}
+
+	public void speedUp(int speed) {
+		this.speed += speed;
+	}
+
+	public void speedDown(int speed) {
+		this.speed -= speed;
+	}
+
+	public void show() {
+		System.out.println("차량명 : " + this.name);
+		System.out.println("차량색 : " + this.color);
+		System.out.println("현재속도 : " + this.speed);
+	}
+}
