c5a5a7c5616458d9532bfc4807f20ad5bb6dec3a
slothfkdla <slothfkdla@gmail.com>
2023-06-22 11:34:47 +0900

    인터페이스

--- Ch05/src/sub9/InterfaceTest.java
+++ Ch05/src/sub9/InterfaceTest.java
@@ -4,7 +4,38 @@ package sub9;
  * 날짜 : 2023/06/22
  * 이름 : 신진성 
  * 내용 : Java 인터페이스 실습하기
+ * 
+ * 인터페이스(Interface)
+ *	- 클래스간의 공통의 표준 규격을 위한 설계 문법
+ *	- 인터페이스를 활용해서 객체간 결합도를 완화하는 효과(다형성)
+ *	- 부수적으로 다중상속 효과
  */
+class Tv extends Computer implements Internet {
+
+	@Override
+	public void access() {
+		surf();
+	}
+
+	public void powerOn() {
+		booting();
+	}
+}
+
+class Computer {
+	public void booting() {
+		System.out.println("시스템 부팅...");
+	}
+
+	public void surf() {
+		System.out.println("인터넷 서핑...");
+	}
+}
+
+interface Internet {
+	public void access();
+}
+
 public class InterfaceTest {
 	public static void main(String[] args) {
 		// 인터페이스 활용1 - 표준화된 클래스 설계와 다형성 활용
@@ -18,9 +49,17 @@ public class InterfaceTest {
 		bongo.speedDown(20);
 		bongo.show();
 
-		// 인터페이스 활용2 - 결합도 완화
+		// 인터페이스 활용2 - 유연성(결합도 완화)
+		Bulb bulb = new Bulb();
+		Socket socket = new Cable(bulb);
+
+		socket.switchOn();
+		socket.switchOff();
 
 		// 인터페이스 활용3 - 다중 상속 효과
-
+		// 업캐스팅 불가능
+		Tv SmartTv = new Tv();
+		SmartTv.powerOn();
+		SmartTv.access();
 	}
 }
---
+++ Ch05/src/sub9/Socket.java
@@ -0,0 +1,7 @@
+package sub9;
+
+public interface Socket {
+	public void switchOn();
+
+	public void switchOff();
+}
