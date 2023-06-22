e1aa622ff1422f19f9518d5671a4f4c632d9c793
slothfkdla <slothfkdla@gmail.com>
2023-06-22 15:04:34 +0900

    백준 8장 완료

--- CodingTest/src/step8/Test08.java
+++ CodingTest/src/step8/Test08.java
@@ -1,5 +1,14 @@
 package step8;
 
-public class Test08 {
+import java.math.BigInteger;
+import java.util.Scanner;
 
+public class Test08 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		System.out.println(a.add(b));
+		sc.close();
+	}
 }
